986a3c5051c63a1d1f5a51e92c94c80f08185e52
Jonathan <jonathan@elmgren.dev>
2023-02-16 19:42:25 +0100

    make sure acf loads in child theme

--- functions/hooks/acf.php
+++ functions/hooks/acf.php
@@ -23,3 +23,23 @@ add_filter('acf/settings/show_admin', 'elmgren_show_acf_settings');
 
 // Hide the ACF Updates menu
 add_filter('acf/settings/show_updates', '__return_false', 100);
+
+// Load json to child theme
+function elmgren_load_acf_json_from_child()
+{
+    return get_stylesheet_directory() . '/acf-json';
+}
+add_filter('acf/settings/save_json', 'elmgren_load_acf_json_from_child');
+
+// Save json to child theme
+function elmgren_save_acf_json_to_child($paths)
+{
+    $paths = array(get_template_directory() . '/acf-json');
+
+    if (is_child_theme()) {
+        $paths[] = get_stylesheet_directory() . '/acf-json';
+    }
+
+    return $paths;
+}
+add_filter('acf/settings/load_json', 'elmgren_save_acf_json_to_child');
