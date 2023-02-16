b6bd45807a518e7f19c3e286d9b4dfb3b7b4cc0c
Jonathan <jonathan@elmgren.dev>
2023-02-16 19:17:04 +0100

    fix acf to go with parent theme and not child theme

--- functions/_consts.php
+++ functions/_consts.php
@@ -1,5 +1,5 @@
 <?php
 
 // Define path and URL to the ACF plugin.
-define('ELMGREN_ACF_PATH', get_stylesheet_directory() . '/plugins/acf/');
-define('ELMGREN_ACF_URL', get_stylesheet_directory_uri() . '/plugins/acf/');
+define('ELMGREN_ACF_PATH', get_template_directory() . '/plugins/acf/');
+define('ELMGREN_ACF_URL', get_template_directory_uri() . '/plugins/acf/');
