b8fc5780efacb7ae267ef147e04b5b7c417f9a0e
Jonathan Elmgren <jonathan@elmgren.dev>
2023-08-10 23:02:30 +0200

    fix inline svgs and optimize footer code

--- functions/functions.php
+++ functions/functions.php
@@ -6,3 +6,17 @@ function get_custom_logo_url()
     $image = wp_get_attachment_image_src($custom_logo_id, 'full');
     return $image[0];
 }
+
+function get_inline_svg($filename)
+{
+    // Define SVG path
+    $svg_path = get_template_directory() . '/assets/images/icons/' . $filename . '.svg';
+    // Check if the SVG file exists
+    if (file_exists($svg_path)) {
+        // Fetch and return the content of the SVG
+        echo file_get_contents($svg_path);
+    } else {
+        // Return a warning (or nothing) if the SVG is not found
+        echo '<!-- SVG not found -->';
+    }
+}
--- header.php
+++ header.php
@@ -4,75 +4,74 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width" />
-    <meta name="description" content="">
     <?php wp_head(); ?>
 </head>
 
+<?php
+// Function to display either the logo or the blog name
+function get_logo_or_blog_name()
+{
+    $home_url = home_url();
+    $blog_name = get_bloginfo('name');
+    $logo_url = esc_url(wp_get_attachment_image_url(get_theme_mod('custom_logo'), 'full'));
+    if (has_custom_logo()) {
+        return '<a href="' . $home_url . '" class="-m-1.5 p-1.5">
+                    <span class="sr-only">' . $blog_name . '</span>
+                    <img style="height: ' . get_logo_height() . 'rem" class="w-auto" src="' . $logo_url . '" alt="logo of ' . esc_attr($blog_name) . '">
+                </a>';
+    } else {
+        return '<a href="' . $home_url . '" class="-m-1.5 p-1.5">' . $blog_name . '</a>';
+    }
+}
+
+// Function to display the main menu
+function get_main_menu()
+{
+    wp_nav_menu([
+        'theme_location' => 'main-menu',
+        'walker' => new Elmgren_Walker_Nav_Menu(),
+        'container' => false,
+        'items_wrap' => '%3$s',
+    ]);
+}
+?>
+
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <header style="<?php echo elmgren_get_header_absolute() ?>" class="bg-white" role="banner">
+    <header style="<?php echo elmgren_get_header_absolute() ?>" role="banner">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a href="<?php echo home_url(); ?>" class="-m-1.5 p-1.5">
-                    <?php if (has_custom_logo()) : ?>
-                        <span class="sr-only"><?php echo get_bloginfo('name'); ?></span>
-                        <img style="height: <?php echo get_logo_height() ?>rem" class="w-auto" src="<?php echo esc_url(wp_get_attachment_image_url(get_theme_mod('custom_logo'), 'full')); ?>" alt="logo of <?php echo esc_attr(get_bloginfo('name')); ?>">
-                    <?php else : ?>
-                        <?php echo get_bloginfo('name'); ?>
-                    <?php endif; ?>
-                </a>
+                <?= get_logo_or_blog_name(); ?>
             </div>
             <div class="flex lg:hidden">
                 <button type="button" data-menu-toggle="open" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
                     <span class="sr-only"><?php _e('Open main menu', 'elmgren') ?></span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
+                    <?php echo get_inline_svg('hamburger_open') ?>
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-                <?php wp_nav_menu([
-                    'theme_location' => 'main-menu',
-                    'walker' => new Elmgren_Walker_Nav_Menu(),
-                    'container' => false,
-                    'items_wrap' => '%3$s',
-                ]); ?>
+                <?php get_main_menu(); ?>
             </div>
         </nav>
-        <!-- Mobile menu, show/hide based on menu open state. -->
+        <!-- Mobile menu -->
         <div class="lg:hidden hidden" role="dialog" aria-modal="true" data-menu="mobile">
-            <!-- Background backdrop, show/hide based on slide-over state. -->
             <div data-backdrop class="fixed inset-0 z-10"></div>
             <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                    <a href="<?php echo home_url(); ?>" class="-m-1.5 p-1.5">
-                        <?php if (has_custom_logo()) : ?>
-                            <span class="sr-only"><?php echo get_bloginfo('name'); ?></span>
-                            <img style="height: <?php echo get_logo_height() ?>rem" class="w-auto" src="<?php echo esc_url(wp_get_attachment_image_url(get_theme_mod('custom_logo'), 'full')); ?>" alt="logo of <?php echo esc_attr(get_bloginfo('name')); ?>">
-                        <?php else : ?>
-                            <?php echo get_bloginfo('name'); ?>
-                        <?php endif; ?>
-                    </a>
+                    <?= get_logo_or_blog_name(); ?>
                     <button data-menu-toggle="close" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                         <span class="sr-only"><?php _e('Close menu', 'elmgren') ?></span>
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                        <?php get_inline_svg('hamburger_close') ?>
                     </button>
                 </div>
                 <div class="mt-6 flow-root">
                     <div class="-my-6 divide-y divide-gray-500/10">
                         <div class="space-y-2 py-6">
-                            <?php wp_nav_menu([
-                                'theme_location' => 'main-menu',
-                                'walker' => new Elmgren_Walker_Nav_Menu(),
-                                'container' => false,
-                                'items_wrap' => '%3$s',
-                            ]); ?>
+                            <?php get_main_menu(); ?>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </header>
-    <main id="content" role="main" class='<?php elmgren_get_page_width() ?>'>
+    <main id="content" role="main" class='<?= elmgren_get_page_width() ?>'>
