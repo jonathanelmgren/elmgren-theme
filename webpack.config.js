a2bf263e15b5ed7aeb259a61e5c6277ec2ba708a
Jonathan Elmgren <jonathan@elmgren.dev>
2023-08-11 00:23:02 +0200

    conditionally load browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,35 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const plugins = [
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+    })
+]
+
+if (typeof process.env.WORDPRESS_SITE_URL === 'string') {
+    plugins.push(
+        new BrowserSyncPlugin({
+            https: {
+                key: 'docker/ssl/server.key',
+                cert: 'docker/ssl/server.crt',
+            },
+            host: process.env.WORDPRESS_SITE_URL.replace('https://', ''),
+            open: "external",
+            port: 3000,
+            proxy: process.env.WORDPRESS_SITE_URL + '/',
+            files: [
+                './header.php',
+                './footer.php',
+                './templates/*.php',
+            ],
+            reloadDelay: 0,
+            injectChanges: true,
+            notify: false
+        })
+    )
+}
+
 module.exports = {
     entry: {
         main: ['./assets/ts/plugins.ts', './assets/scss/main.scss']
@@ -40,29 +69,7 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].css'
-        }),
-        new BrowserSyncPlugin({
-            https: {
-                key: 'docker/ssl/server.key',
-                cert: 'docker/ssl/server.crt',
-            },
-            host: process.env.WORDPRESS_SITE_URL.replace('https://',''),
-            open: "external",
-            port: 3000,
-            proxy: process.env.WORDPRESS_SITE_URL + '/',
-            files: [
-                './header.php',
-                './footer.php',
-                './templates/*.php',
-            ],
-            reloadDelay: 0,
-            injectChanges: true,
-            notify: false
-        })
-    ],
+    plugins,
     resolve: {
         extensions: ['.tsx', '.ts', '.js', '.scss'],
     },
