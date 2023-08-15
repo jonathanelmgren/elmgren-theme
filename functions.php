4a8751a5ee2f27f1c2ad377aafd09bc4caf78ad3
Jonathan Elmgren <jonathan@elmgren.dev>
2023-08-15 09:59:15 +0200

    remove old require

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ if (\file_exists(get_template_directory() . '/vendor/autoload.php')) {
 
 require_once 'functions/constants.php';
 require_once 'functions/setup.php';
-require_once 'plugins/acf/acf.php';
 
 elm_include_folder('classes');
 elm_include_folder('functions');
