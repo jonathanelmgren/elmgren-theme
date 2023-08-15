a659db9bf98b94a11c6fdaba6d41950b1a41f905
Jonathan Elmgren <jonathan@elmgren.dev>
2023-08-15 09:57:45 +0200

    remove old files

--- functions/settings/options_page.php
+++ functions/settings/options_page.php
@@ -1,5 +1,6 @@
 <?php
 
+// Add the page
 if (function_exists('acf_add_options_page')) {
     acf_add_options_page(array(
         'page_title'    => 'Theme Settings',
@@ -9,3 +10,91 @@ if (function_exists('acf_add_options_page')) {
         'redirect'      => false
     ));
 }
+
+// Add the fields
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_63ece9aedfed8',
+        'title' => 'Theme Settings',
+        'fields' => array(
+            array(
+                'key' => 'field_64d1590e5a860',
+                'label' => 'BETA',
+                'name' => 'beta',
+                'aria-label' => '',
+                'type' => 'group',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'layout' => 'block',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_64d1591d5a861',
+                        'label' => 'Access beta versions of Elmgren Theme',
+                        'name' => 'allow_beta_parent',
+                        'aria-label' => '',
+                        'type' => 'true_false',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'message' => '',
+                        'default_value' => 0,
+                        'ui' => 0,
+                        'ui_on_text' => '',
+                        'ui_off_text' => '',
+                    ),
+                    array(
+                        'key' => 'field_64d159445a862',
+                        'label' => 'Access beta versions of child theme',
+                        'name' => 'allow_beta_child',
+                        'aria-label' => '',
+                        'type' => 'true_false',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'message' => '',
+                        'default_value' => 0,
+                        'ui' => 0,
+                        'ui_on_text' => '',
+                        'ui_off_text' => '',
+                    ),
+                ),
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'theme-general-settings',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+        'show_in_rest' => 0,
+    ));
+
+endif;
